954601606864abc56a382d8f60c18268dd5201a0
jjlebow@gmail.com <jlebow@uci.edu>
2019-12-05 18:20:41 -0800

    added backgroundsd amd a working menu scene

--- Assets/Scripts - General/MainMenu.cs
+++ Assets/Scripts - General/MainMenu.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{
-
-    public void PlayGame()
+{   
+    public void Playgame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-	
-	public void QuitGame() {
-        Application.Quit();
-	}
 
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
 }
